3ba59657701abe7fad4b5fd87968fe2d5fcf2ac2
Basil Arif <basilarif@basils-macbook-air.local>
2020-04-07 17:32:28 -0400

    Initial Commit

---
+++ grocery-go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grocery-go
+//
+//  Created by Basil Arif on 2020-04-07.
+//  Copyright © 2020 Basil Arif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ grocery-goTests/grocery_goTests.swift
@@ -0,0 +1,34 @@
+//
+//  grocery_goTests.swift
+//  grocery-goTests
+//
+//  Created by Basil Arif on 2020-04-07.
+//  Copyright © 2020 Basil Arif. All rights reserved.
+//
+
+import XCTest
+@testable import grocery_go
+
+class grocery_goTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
